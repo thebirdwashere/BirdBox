e21ed74bb7e7f12e3a1e4437fb09c882d319c5d0
AgentNebulator <mattylaven@gmail.com>
2024-02-20 20:33:01 -0600

    Added customizable number of faces to diceroll

--- cmds/diceroll.js
+++ cmds/diceroll.js
@@ -2,9 +2,29 @@ const { randomIntInRange } = require("../utils")
 
 module.exports = {
   name: 'diceroll',
-  description: 'diceroll, what else could it be?',
-  execute(message){
-    const result = randomIntInRange(1, 6)
-    message.tryreply(`:game_die: You rolled a ${result}!`);
+  description: 'command to roll a die and get a random result',
+  execute(message, args){
+
+    const numberOfFaces = getNumberOfFaces(args[0])
+    if (!numberOfFaces) {return;}
+
+    const result = randomIntInRange(1, numberOfFaces);
+    message.tryreply(`:game_die: You rolled ${result}!`);
   }
+}
+
+function getNumberOfFaces(providedNum) {
+  let faces = Number(providedNum);
+
+  if (faces < 1) {
+    message.tryreply(`bro i kinda need a positive number of faces`);
+    return null;
+  } else if (faces !== Math.floor(faces)) {
+    message.tryreply(`bro i kinda need an integer number of faces`);
+    return null;
+  } else if (!faces) {
+    faces = 6;
+  }
+
+  return faces
 }
