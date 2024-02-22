1826289f0a98bdeaae0b9e23c9fa98adf5872b6c
AgentNebulator <mattylaven@gmail.com>
2024-02-21 21:32:10 -0600

    Fixed crash when announcing to deleted channels

--- cmds/announce.js
+++ cmds/announce.js
@@ -4,12 +4,12 @@ module.exports = {
     async execute(message, args, vars){
         let announce_channel = await vars.db.get(`setting_announce_channel_${message.guildId}`)
         
-        await message.guild.channels.fetch(announce_channel)
+        try {await message.guild.channels.fetch(announce_channel)
             .then(channel => announce_channel = channel)
-            .catch();
+        } catch {message.channel.trysend(`announcements disabled currently, use ${vars.prefix}config to enable it`); return;}
         
         if (!announce_channel.id) {message.channel.trysend(`announcements disabled currently, use ${vars.prefix}config to enable it`); return;}
         
-        announce_channel.send(message.content.replace(`${vars.prefix}announce`, ''));
+        announce_channel.trysend(message.content.replace(`${vars.prefix}announce`, '').trim());
     }
 }
