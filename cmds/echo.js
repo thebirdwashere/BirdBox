7cb9f59f904fb19392947b268a3234e1935a3e4a
AgentNebulator <mattylaven@gmail.com>
2024-02-21 21:33:08 -0600

    Fixed crash in echo when no argument

--- cmds/echo.js
+++ cmds/echo.js
@@ -2,10 +2,10 @@ module.exports = {
     name: 'echo',
     description: "echo command",
     execute(message, args, vars){
-        if (args[0].trim() == "noreply") {
-            message.channel.trysend(message.content.replace(`${vars.prefix}echo noreply`, ''));
+        if (args[0]?.trim() == "noreply") {
+            message.channel.trysend(message.content.replace(`${vars.prefix}echo noreply`, '').trim());
         } else {
-            message.tryreply(message.content.replace(`${vars.prefix}echo`, ''));
+            message.tryreply(message.content.replace(`${vars.prefix}echo`, '').trim());
         }   
     }
 }
