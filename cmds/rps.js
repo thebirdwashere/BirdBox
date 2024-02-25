107635fe4c9576d8dce9314f52fc30b2d8ad8531
AgentNebulator <mattylaven@gmail.com>
2024-02-24 20:46:41 -0600

    Replaced rps choices will full versions so message is clearer

--- cmds/rps.js
+++ cmds/rps.js
@@ -9,35 +9,40 @@ module.exports = {
 	description: 'Play a game of Rock Paper Scissors against the computer!',
 	execute({message, args}) {
 		const validChoices = ['rock', 'paper', 'scissors', 'r', 'p', 's'];
-		const playerChoice = args[0];
+		let playerChoice = args[0];
+		
+		if(!validChoices.includes(playerChoice)) { message.channel.trysend('Invalid move. Try again and use "rock", "paper", or "scissors".'); return;}
 
-		if(!validChoices.includes(playerChoice)) { message.channel.trysend('Invalid move. Try again and use \"rock\", \"paper\", or \"scissors\".'); return;}
-			const computerNumChoice =  randomIntInRange(0, 2);
+		//replace choices with full versions so the logic is cleaner and the later message makes sense
+		const abbreviatedChoices = {r: "rock", p: "paper", s: "scissors"}
+		abbreviatedChoices[playerChoice] && (playerChoice = abbreviatedChoices[playerChoice])
 
-			/*
-			 *	0 = rock
-			 *	1 = paper
-			 *	2 = scissors
-			*/
+		const computerNumChoice =  randomIntInRange(0, 2);
 
-			const computerChoice = validChoices[computerNumChoice]
-			let playerNumChoice;
+		/*
+		*	0 = rock
+		*	1 = paper
+		*	2 = scissors
+		*/
 
-			switch(playerChoice) {
-				case 'r': case 'rock':
-					playerNumChoice = 0; break;
-				case 'paper': case 'p':
-					playerNumChoice = 1; break;
-				case 's': case 'scissors':
-					playerNumChoice = 2; break;
-			}
+		const computerChoice = validChoices[computerNumChoice]
+		let playerNumChoice;
 
-			if (playerNumChoice == computerNumChoice) {var result = 'tied';}
-			else if (((playerNumChoice - computerNumChoice) + 3) % 3 == 1) {var result = 'won';} //this works, trust me bro, stackoverlow told me it did
-			else {var result = 'lost';}
-
-
-
-			message.tryreply(`Congrats, you ${result}! The computer selected ${computerChoice} and you selected ${playerChoice}.`);
+		switch(playerChoice) {
+			case 'rock':
+				playerNumChoice = 0; break;
+			case 'paper':
+				playerNumChoice = 1; break;
+			case 'scissors':
+				playerNumChoice = 2; break;
 		}
+
+		if (playerNumChoice == computerNumChoice) {var result = 'tied';}
+		else if (((playerNumChoice - computerNumChoice) + 3) % 3 == 1) {var result = 'won';} //this works, trust me bro, stackoverlow told me it did
+		else {var result = 'lost';}
+
+
+
+		message.tryreply(`Congrats, you ${result}! The computer selected ${computerChoice} and you selected ${playerChoice}.`);
+	}
 }
