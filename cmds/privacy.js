1f0f48cd1c6030165cdfbc1b7ad7662614cd9129
AgentNebulator <mattylaven@gmail.com>
2024-02-24 18:45:05 -0600

    Changed privacy policy embed to use title link

--- cmds/privacy.js
+++ cmds/privacy.js
@@ -9,9 +9,7 @@ module.exports = {
 			.setTitle('Privacy Policy')
 			.setAuthor({ name: 'BirdBox', iconURL: 'https://cdn.discordapp.com/avatars/803811104953466880/5bce4f0ba438015ec65f5b9cac11c8e3.png?size=256' })
 			.setDescription('Click the link for more information about the data BirdBox stores, how it is stored, and the license BirdBox uses.')
-			.addFields(
-				{ name: 'Link', value: 'https://thebirdwashere.thejasperhouse.net/birdbox/privacy.html' }
-			);
+			.setURL('https://thebirdwashere.thejasperhouse.net/birdbox/privacy.html');
         
 		message.channel.trysend({ embeds: [creditsEmbed] });
 	}
