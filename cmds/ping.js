d8d65c39aeebe5396d3b7dc240dfd02dace04221
AgentNebulator <mattylaven@gmail.com>
2024-03-02 19:53:06 -0600

    ported bisly's ping overhaul to main

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,7 +1,16 @@
+const { EmbedBuilder } = require("discord.js");
+
 module.exports = {
     name: 'ping',
     description: "ping pong command",
-    execute({message}){
-        message.channel.trysend('pong you bumbling pillock');
+    execute({message}, {client}){
+		const pingEmbed = new EmbedBuilder()
+			.setColor(0x0282EC)
+			.addFields(
+				{ name: 'Ping Time', value: `${client.ws.ping}ms`}
+			)
+			.setFooter({ text: 'pong you bumbling pillock' });
+
+        message.reply({ embeds: [pingEmbed] });
     }
 }
