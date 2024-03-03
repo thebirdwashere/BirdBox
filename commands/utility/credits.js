293e56efd347a7e5b67496a36a31394a8f045300
bislij <funkybislij@gmail.com>
2024-03-02 22:04:41 -0600

    added credits command

---
+++ commands/utility/credits.js
@@ -0,0 +1,24 @@
+const devArray = require('../../utils/json_info/dev_array.json');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+		.setName('credits')
+		.setDescription('Credits to the people who work/worked on this bot.'),
+    async execute(interaction, {embedColor}) {
+
+        const creditsEmbed = new EmbedBuilder()
+			.setColor(embedColor)
+			.setTitle('Credits')
+			.setDescription('Credits to the people who work/worked on this bot.')
+			.addFields(
+				{ name: 'Host', value: devArray.host[0].name },
+				{ name: 'Developers', value: devArray.developer.map(item => item.name).join(", ") },
+				{ name: 'Bot Testers', value: devArray.botTester.map(item => item.name).join(", ") }
+			)
+			.setFooter({ text: 'Special thanks to users like you!' });
+
+        await interaction.reply({ embeds: [creditsEmbed] });
+        
+    }
+}
