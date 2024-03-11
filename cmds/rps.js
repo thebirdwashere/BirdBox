e5ed7d8739a4870ee201a07ebdca248d27d1bd15
AgentNebulator <mattylaven@gmail.com>
2024-03-10 21:08:42 -0500

    Added comments to rps

--- cmds/rps.js
+++ cmds/rps.js
@@ -12,37 +12,33 @@ module.exports = {
 		const validChoices = ['rock', 'paper', 'scissors', 'r', 'p', 's'];
 		let playerChoice = args[0];
 		
+		//make sure they added a correct choice
 		if(!validChoices.includes(playerChoice)) { message.channel.trysend('bruh it\'s literally the title of the game, you gotta use "rock", "paper", or "scissors".'); return;}
 
 		//replace choices with full versions so the logic is cleaner and the later message makes sense
 		const emojifiedChoices = {r: ":rock:", rock: ":rock:", p: ":roll_of_paper:", paper: ":roll_of_paper:", s: ":scissors:", scissors: ":scissors:"};
 		if (emojifiedChoices[playerChoice]) playerChoice = emojifiedChoices[playerChoice]
 
-		const computerNumChoice =  randomIntInRange(0, 2);
+		//pick a random choice from the first three of validChoices, then use the emoji version
+		const computerChoice = emojifiedChoices[validChoices[randomIntInRange(0, 2)]]
 
-		/*
-		*	0 = rock
-		*	1 = paper
-		*	2 = scissors
-		*/
-
-		const computerChoice = emojifiedChoices[validChoices[computerNumChoice]]
-
+		//determine match result based on choices
 		const matchResults = {
 			":rock:": {":rock:": 'Tied', ":roll_of_paper:": 'Lost', ":scissors:": 'Won'},
 			":roll_of_paper:": {":rock:": 'Won', ":roll_of_paper:": 'Tied', ":scissors:": 'Lost'},
 			":scissors:": {":rock:": 'Lost', ":roll_of_paper:": 'Won', ":scissors:": 'Tied'}
 		}
-
 		const result = matchResults[playerChoice][computerChoice]
-        let footer;
 
+		//set footer based on match result
+        let footer;
 		switch (result) {
 			case "Won": footer = 'decent job chump'; break;
 			case "Tied": footer = 'kinda mid game ngl'; break;
 			case "Lost": footer = 'massive L'; break;
 		}
 
+		//create embed and reply with it
         const rpsEmbed = new EmbedBuilder()
             .setTitle(`You ${result}!`)
             .setColor(0x208ddd)
@@ -51,7 +47,7 @@ module.exports = {
                 { name: 'BirdBox', value: `${computerChoice}`, inline: true}
 			)
             .setFooter({ text: footer });
-
+		
         message.reply({ embeds: [rpsEmbed] });
 	}
 }
