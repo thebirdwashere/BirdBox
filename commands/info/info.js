aeafbe8d02e26837b0ae4eba796d098b028f826a
bislij <funkybislij@gmail.com>
2024-03-16 21:35:20 -0500

    added bot description in /info from other branch

--- commands/info/info.js
+++ commands/info/info.js
@@ -11,7 +11,7 @@ module.exports = {
 			.setColor(embedColors.white)
 			.setTitle('Information')
             .setAuthor({ name: 'BirdBox', iconURL: 'https://cdn.discordapp.com/avatars/803811104953466880/5bce4f0ba438015ec65f5b9cac11c8e3.webp' })
-            .setDescription('(FILL THIS OUT TMR) Description of birdbox')
+            .setDescription('A discord bot concentrating on humorous features and interactions, while also providing a couple nice utilities. The original project was created in late January 2021 using Zapier and BotGhost, and was rewritten in November 2021 using discord.js. Primarily a hobby project with pretty slow development, but we hope you get some enjoyment or even use out of it regardless!')
 			.addFields(
                 { name: 'Latest Version', value: patchNotes[0].version },
                 { name: 'Last Updated', value: patchNotes[0].date },
@@ -30,7 +30,7 @@ module.exports = {
 			.setColor(embedColors.white)
 			.setTitle('Information')
             .setAuthor({ name: 'BirdBox', iconURL: 'https://cdn.discordapp.com/avatars/803811104953466880/5bce4f0ba438015ec65f5b9cac11c8e3.webp' })
-            .setDescription('(FILL THIS OUT TMR) Description of birdbox')
+            .setDescription('A discord bot concentrating on humorous features and interactions, while also providing a couple nice utilities. The original project was created in late January 2021 using Zapier and BotGhost, and was rewritten in November 2021 using discord.js. Primarily a hobby project with pretty slow development, but we hope you get some enjoyment or even use out of it regardless!')
 			.addFields(
                 { name: 'Latest Version', value: patchNotes[0].version },
                 { name: 'Last Updated', value: patchNotes[0].date },
