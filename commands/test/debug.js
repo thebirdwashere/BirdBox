12bb43d23d2e6abac6f8180550d95b2f9558807a
bislij <funkybislij@gmail.com>
2024-03-21 17:02:07 -0500

    added subcommand for permission level checking per command

--- commands/test/debug.js
+++ commands/test/debug.js
@@ -3,25 +3,48 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('debug')
-        .setDescription('Grabs values from the database at request.')
-        .addStringOption(option =>
-			option
-				.setName('item')
-				.setDescription('Item that you want to grab from the database.')
-                .setMaxLength(128)
-				.setRequired(true)
+        .setDescription('Various debug functions for BirdBox developers.')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('database')
+                .setDescription('Grabs values from the database at request.')
+                .addStringOption(option => option.setName('item').setDescription('Item that you want to grab from the database.').setRequired(true))
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('permissions')
+                .setDescription('Checks the permissions of a command.')
+                .addStringOption(option => option.setName('command').setDescription('The command to check the permissions of.').setRequired(true))
         ),
     filter: ['host', 'developer'],
-    async execute(interaction, {db}) {
+    async execute(interaction, {db, commands}) {
 
-        const item = interaction.options.getString('item');
-        const database_item = await db.get(item);
-        
-        if (database_item) {
-            let itemstring = JSON.stringify(database_item);
-            itemstring = itemstring.match(/.{1,2000}/g);
-            itemstring.forEach(element => { interaction.reply({ content: element, ephemeral: true }) });
-        } else { interaction.reply({ content: `Failed to locate item: \`${item}\` in the database. Please try again.`, ephemeral: true }) };
+        switch (interaction.options.getSubcommand()) { // Switch to handle different subcommands.
+            case 'database': {
+
+                const item = interaction.options?.getString('item');
+                const database_item = await db.get(item);
+                
+                if (database_item) {
+                    let itemstring = JSON.stringify(database_item);
+                    itemstring = itemstring.match(/.{1,2000}/g);
+                    itemstring.forEach(element => { interaction.reply({ content: `Value: ${element}`, ephemeral: true }) });
+                } else { interaction.reply({ content: `Failed to locate item: \`${item}\` in the database. Please try again.`, ephemeral: true }) };
+
+            } break;
+
+            case 'permissions': {
+
+                const command = interaction.options?.getString('command');
+                if (!commands.map(item => item.data.name).includes(command)) return interaction.reply({ content: 'The specified command does not exist. Please try again.', ephemeral: true });
+
+                const commandPerms = Object.fromEntries( commands.map(item => item.data.name).map((key, index) => [key, commands.map(item => item.filter)[index]]) );
+
+                if (!commandPerms[command]) return interaction.reply({ content: `Valid permission levels of \`/${command}\`: \`(any)\``, ephemeral: true });
+                interaction.reply({ content: `Valid permission levels of \`/${command}\`: ${commandPerms[command].map(item => `\`${item}\``).join(', ')}`, ephemeral: true });
+
+            } break;
+        }
     
     }
 }
