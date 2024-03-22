98674f3682ca4865e6a31751cc466f53590aed50
bislij <funkybislij@gmail.com>
2024-03-22 12:52:58 -0500

    changed reload command to use ephemeral messages

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -25,11 +25,11 @@ module.exports = {
 	        interaction.client.commands.delete(command.data.name);
 	        const newCommand = require(`../${command.category}/${command.data.name}.js`);
 	        interaction.client.commands.set(newCommand.data.name, newCommand);
-	        await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
+	        await interaction.reply({ content: `Command \`${newCommand.data.name}\` was reloaded!`, ephemeral: true });
 		} catch (error) {
 	        console.error(error);
-	        await interaction.reply(`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``);
+	        await interaction.reply({ content: `There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``, ephemeral: true });
 		}
-        
+
 	},
 };
