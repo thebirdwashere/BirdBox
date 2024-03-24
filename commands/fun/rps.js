29474e616dcfb76c0845cd9d99acccacc120d81c
AgentNebulator <mattylaven@gmail.com>
2024-03-24 16:01:06 -0500

    Ported rps to classic mode (may need rewrite is a bit messy)

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -59,5 +59,49 @@ module.exports = {
 
         await interaction.reply({ embeds: [rpsEmbed] });
 
+    },
+    async executeClassic({message, args}, { embedColors }) {
+		const validChoices = ['rock', 'paper', 'scissors', 'r', 'p', 's'];
+		let playerChoice = args[0];
+		
+		//make sure they added a correct choice
+		if(!validChoices.includes(playerChoice)) { message.reply('bruh it\'s literally the title of the game, you gotta use "rock", "paper", or "scissors".').catch(e => console.error(e)); return;}
+
+		//replace choices with full versions so the logic is cleaner and the later message makes sense
+		const emojifiedChoices = {r: ":rock:", rock: ":rock:", p: ":roll_of_paper:", paper: ":roll_of_paper:", s: ":scissors:", scissors: ":scissors:"};
+		if (emojifiedChoices[playerChoice]) playerChoice = emojifiedChoices[playerChoice]
+
+		//pick a random choice from the first three of validChoices, then use the emoji version
+		const computerChoice = emojifiedChoices[validChoices[Math.floor(Math.random() * 2)]]
+
+		//determine match result based on choices
+		const matchResults = {
+			":rock:": {":rock:": 'Tied', ":roll_of_paper:": 'Lost', ":scissors:": 'Won'},
+			":roll_of_paper:": {":rock:": 'Won', ":roll_of_paper:": 'Tied', ":scissors:": 'Lost'},
+			":scissors:": {":rock:": 'Lost', ":roll_of_paper:": 'Won', ":scissors:": 'Tied'}
+		}
+		const result = matchResults[playerChoice][computerChoice]
+
+        let selectedColor;
+
+		//set footer based on match result
+        let footer;
+		switch (result) {
+			case "Won": footer = 'decent job chump'; selectedColor = embedColors.green; break;
+			case "Tied": footer = 'kinda mid game ngl'; selectedColor = embedColors.yellow; break;
+			case "Lost": footer = 'massive L'; selectedColor = embedColors.red; break;
+		}
+
+		//create embed and reply with it
+        const rpsEmbed = new EmbedBuilder()
+            .setTitle(`You ${result}!`)
+            .setColor(selectedColor)
+            .addFields(
+				{ name: 'You', value: `${playerChoice}`, inline: true},
+                { name: 'BirdBox', value: `${computerChoice}`, inline: true}
+			)
+            .setFooter({ text: footer });
+		
+        message.reply({ embeds: [rpsEmbed] }).catch(e => console.error(e));
     }
 }
