a6882a153bebbe4790ad7efc72cd80436b63f7d7
AgentNebulator <mattylaven@gmail.com>
2024-03-24 21:22:51 -0500

    Ported cats command to classic

--- commands/fun/cats.js
+++ commands/fun/cats.js
@@ -30,6 +30,23 @@ module.exports = {
 
         await interaction.reply({ embeds: [catEmbed] });
       } catch (error) { await interaction.reply({ content: `There was an error: \`${error}\`. This was most likely caused by a cooldown or timeout. Consider slowing down your request rate.`, ephemeral: true }); }
+    },
+    async executeClassic({message, args}, {embedColors}) {
 
+      const imageType = args[0] || 'jpg';
+
+      try {
+        let catData; await fetch(`https://api.thecatapi.com/v1/images/search?mime_types=${imageType}`)
+        .then(response => response.json())
+        .then(json => {catData = json})
+
+        const catEmbed = new EmbedBuilder()
+          .setTitle(`Random Cat`)
+          .setAuthor({ name: 'TheCatAPI', iconURL: 'https://i.natgeofe.com/n/eb0f9db1-8b29-4598-ad7e-89716501189f/cat-whisperers-nationalgeographic_1048225_square.jpg' })
+          .setColor(embedColors.blue)
+          .setImage(catData[0].url);
+
+        await message.reply({ embeds: [catEmbed] });
+      } catch (error) { await message.reply({ content: `There was an error: \`${error}\`. This was most likely caused by a cooldown or timeout. Consider slowing down your request rate.` }); }
     }
 }
