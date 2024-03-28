e53209fa165b3894897599523663b2c4c2a8a904
AgentNebulator <mattylaven@gmail.com>
2024-03-27 21:11:17 -0500

    Ported credits to classic

--- commands/info/credits.js
+++ commands/info/credits.js
@@ -21,5 +21,23 @@ module.exports = {
 
         await interaction.reply({ embeds: [creditsEmbed] });
         
+    },
+	async executeClassic({message}, {embedColors, devs}) {
+
+        const creditsEmbed = new EmbedBuilder()
+			.setColor(embedColors.white)
+			.setTitle('Credits')
+			.setDescription('Credits to the people who work/worked on this bot.')
+			.addFields(
+				{ name: 'Host', value: devs.host[0].name },
+				{ name: 'Developers', value: devs.developer.map(item => item.name).join(", ") },
+				{ name: 'Contributors', value: devs.contributor.map(item => item.name).join(", ") },
+				{ name: 'Bot Testers', value: devs.botTester.map(item => item.name).join(", ") },
+				{ name: 'Other', value: devs.misc.map(item => item.name).join(", ") }
+			)
+			.setFooter({ text: 'Special thanks to users like you!' });
+
+        await message.reply({ embeds: [creditsEmbed] });
+        
     }
 }
