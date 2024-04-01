3392dd63aa319b7ef9effd819dc7f9a5eecc7dfd
AgentNebulator <mattylaven@gmail.com>
2024-03-31 20:34:25 -0500

    Added leaderboard to flags

--- commands/fun/flags.js
+++ commands/fun/flags.js
@@ -1,5 +1,5 @@
 const { sampleArray, shuffleArray, sleep } = require("../../utils/scripts/util_scripts.js");
-const flags = require("../../utils/json/flags.json");
+const { flags, difficulties } = require("../../utils/json/flags.json");
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require("discord.js");
 
 module.exports = {
@@ -10,138 +10,243 @@ module.exports = {
             subcommand
                 .setName('quiz')
                 .setDescription('Given a country, guess its flag.')
+                .addStringOption(option =>
+                    option
+                        .setName('difficulty')
+                        .setDescription('Change how many flags are available to guess.')
+                        .addChoices(
+                            { name: `${difficulties[0].name} (${difficulties[0].points} points)`, value: "0" },
+                            { name: `${difficulties[1].name} (${difficulties[1].points} points)`, value: "1" },
+                            { name: `${difficulties[2].name} (${difficulties[2].points} points)`, value: "2" },
+                            { name: `${difficulties[3].name} (${difficulties[3].points} points)`, value: "3" },
+                            { name: `${difficulties[4].name} (${difficulties[4].points} points)`, value: "4" }
+                        )
+                )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('leaderboard')
+                .setDescription('View high scores across all BirdBox users.')
         ),
-    async execute(interaction, {embedColors}) {
+    async execute(interaction, {client, embedColors, db}) {
 
-        const countryNames = Object.keys(flags)
-        const countryFlags = Object.values(flags)
+        switch (interaction.options.getSubcommand()) { // Switch to handle different subcommands.
+            case 'quiz': {
+                const difficulty = difficulties[interaction.options?.getString('difficulty')] ?? difficulties[0]
+                const flagsNum = difficulty.flags;
 
-        const guessFlags = sampleArray(countryFlags, 4)
-        const rightFlag = guessFlags[0]
-        const rightCountry = countryNames[countryFlags.indexOf(rightFlag)]
+                const countryNames = Object.keys(flags);
+                const countryFlags = Object.values(flags);
         
-        const shuffledFlags = shuffleArray(guessFlags)
-
-        let remainingTime = 15
-        let peopleGuessed = 0
-
-        const flagEmbed = new EmbedBuilder()
-        .setTitle(`What is the flag of ${rightCountry}?`)
-        flagEmbed.setFooter({text: `${peopleGuessed} guessed ● ${remainingTime} seconds left`})
-        .setColor(embedColors.blue)
-
-        const flagButton1 = new ButtonBuilder()
-        .setCustomId(shuffledFlags[0])
-        .setEmoji(shuffledFlags[0])
-        .setStyle(ButtonStyle.Primary);
-
-        const flagButton2 = new ButtonBuilder()
-        .setCustomId(shuffledFlags[1])
-        .setEmoji(shuffledFlags[1])
-        .setStyle(ButtonStyle.Primary);
-
-        const flagButton3 = new ButtonBuilder()
-        .setCustomId(shuffledFlags[2])
-        .setEmoji(shuffledFlags[2])
-        .setStyle(ButtonStyle.Primary);
-
-        const flagButton4 = new ButtonBuilder()
-        .setCustomId(shuffledFlags[3])
-        .setEmoji(shuffledFlags[3])
-        .setStyle(ButtonStyle.Primary);
-
-        const buttonRow = new ActionRowBuilder()
-            .addComponents(flagButton1, flagButton2, flagButton3, flagButton4)
-
-        const response = await interaction.reply({embeds: [flagEmbed], components: [buttonRow]})
-
-        const buttonCollector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 15000 });
-
-        let correctUsers = []
-        let wrongUsers = []
-
-        buttonCollector.on('collect', async i => {
-            const userPing = `<@${i.member.id}>`
-            if (i.customId == rightFlag && !correctUsers.includes(userPing)) {
-                if (wrongUsers.includes(userPing)) {
-                    wrongUsers.splice(wrongUsers.indexOf(userPing), 1)
-                }
+                const guessFlags = sampleArray(countryFlags, flagsNum);
+                const rightFlag = guessFlags[0];
+                const rightCountry = countryNames[countryFlags.indexOf(rightFlag)];
                 
-                correctUsers.push(userPing)
-            }
+                const shuffledFlags = shuffleArray(guessFlags);
+        
+                let remainingTime = 15;
+                let peopleGuessed = 0;
+        
+                const flagEmbed = new EmbedBuilder()
+                .setTitle(`What is the flag of ${rightCountry}?`)
+                flagEmbed.setFooter({text: `${peopleGuessed} guessed ● ${remainingTime} seconds left`})
+                .setColor(embedColors.blue);
+        
+                let buttonRowArray = []
 
-            if (i.customId != rightFlag && !wrongUsers.includes(userPing)) {
-                if (correctUsers.includes(userPing)) {
-                    correctUsers.splice(correctUsers.indexOf(userPing), 1)
+                for (let i = 0; i < flagsNum; i++) {
+                    const buttonRowNum = Math.floor(i/4)
+
+                    if (!buttonRowArray[buttonRowNum]) {
+                        buttonRowArray[buttonRowNum] = new ActionRowBuilder()
+                    }
+                    
+                    buttonRowArray[buttonRowNum].addComponents(
+                        new ButtonBuilder()
+                        .setCustomId(shuffledFlags[i])
+                        .setEmoji(shuffledFlags[i])
+                        .setStyle(ButtonStyle.Primary)
+                    )
                 }
-                wrongUsers.push(userPing)
+        
+                const response = await interaction.reply({embeds: [flagEmbed], components: buttonRowArray})
+        
+                const buttonCollector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 15000 });
+        
+                let correctUsers = []
+                let wrongUsers = [interaction.member.id] //automatically a loser, just in case you don't answer
+        
+                buttonCollector.on('collect', async i => {
+                    const userId = i.member.id
+                    if (i.customId == rightFlag && !correctUsers.includes(userId)) {
+                        if (wrongUsers.includes(userId)) {
+                            wrongUsers.splice(wrongUsers.indexOf(userId), 1)
+                        }
+                        
+                        correctUsers.push(userId)
+                    }
+        
+                    if (i.customId != rightFlag && !wrongUsers.includes(userId)) {
+                        if (correctUsers.includes(userId)) {
+                            correctUsers.splice(correctUsers.indexOf(userId), 1)
+                        }
+                        wrongUsers.push(userId)
+                    }
+        
+                    peopleGuessed = correctUsers.length + wrongUsers.length
+                    flagEmbed.setFooter({text: `${peopleGuessed} guessed ● ${remainingTime} seconds left`})
+                    
+                    await response.edit({embeds: [flagEmbed]})
+                    i.deferUpdate()
+                });
+        
+                buttonCollector.on('end', async () => {
+        
+                    buttonRowArray.forEach(row => {
+                        row.components.forEach(async button => {
+                            await button
+                                .setStyle(ButtonStyle.Danger)
+                                .setDisabled(true)
+                        })
+                    })
+        
+                    const rightFlagIndex = shuffledFlags.indexOf(rightFlag)
+                    await buttonRowArray[Math.floor(rightFlagIndex / 4)].components[rightFlagIndex % 4].setStyle(ButtonStyle.Success)
+        
+                    await response.edit({embeds: [flagEmbed], components: buttonRowArray})
+                    
+                    const pointsEarned = difficulty.earned
+                    const pointsLost = difficulty.lost
+        
+                    const usersFormatter = new Intl.ListFormat("en", { type: "conjunction" })
+                    const correctUserString = usersFormatter.format(correctUsers.map(userId => `<@${userId}>`))
+                    const wrongUserString = usersFormatter.format(wrongUsers.map(userId => `<@${userId}>`))
+        
+                    let responseText = ""
+        
+                    switch (correctUsers.length) {
+                        case 0:
+                            responseText += `Nobody got it right! \n`
+                            break;
+                        case 1:
+                            responseText += `${correctUserString} got it right! gg *(+${pointsEarned} points)*\n`
+                            break;
+                        case 2:
+                            responseText += `${correctUserString} both got it right! gg *(+${pointsEarned} points)*\n`
+                            break;
+                        default:
+                            responseText += `${correctUserString} all got it right! gg *(+${pointsEarned} points)*\n`
+                            break;
+                    }
+                    switch (wrongUsers.length) {
+                        case 0:
+                            responseText += `That means nobody got it wrong... pretty good ig`
+                            break;
+                        case 1:
+                            responseText += `That means ${wrongUserString} got it wrong, massive L *(-${pointsLost} points)*`
+                            break;
+                        case 2:
+                            responseText += `That means ${wrongUserString} both got it wrong, massive L *(-${pointsLost} points)*`
+                            break;
+                        default:
+                            responseText += `That means ${wrongUserString} all got it wrong, massive L *(-${pointsLost} points)*`
+                            break;
+                    }
+                    
+                    await interaction.followUp(responseText)
+        
+                    for (const userId of correctUsers) {
+                        let userStats = await db.get(`flags_stats.flag_quiz.${userId}`);
+        
+                        if (!userStats) userStats = {
+                                wins: 0,
+                                losses: 0,
+                                current_streak: 0,
+                                best_streak: 0,
+                                points: 0
+                        }
+        
+                        userStats.wins++;
+                        userStats.current_streak++;
+                        userStats.points += pointsEarned;
+        
+                        if (userStats.current_streak > userStats.best_streak) {
+                            userStats.best_streak = userStats.current_streak;
+                        }
+        
+                        await db.set(`flags_stats.flag_quiz.${userId}`, userStats);
+                    }
+        
+                    for (const userId of wrongUsers) {
+                        let userStats = await db.get(`flags_stats.flag_quiz.${userId}`);
+        
+                        if (!userStats) userStats = {
+                            wins: 0,
+                            losses: 0,
+                            current_streak: 0,
+                            best_streak: 0,
+                            points: 0
+                        }
+                        
+                        userStats.losses++;
+                        userStats.current_streak = 0;
+                        userStats.points -= pointsLost;
+        
+                        await db.set(`flags_stats.flag_quiz.${userId}`, userStats);
+                    }
+                    
+                });
+        
+                while (remainingTime) {
+                    await sleep(5000)
+        
+                    remainingTime -= 5
+                    flagEmbed.setFooter({text: `${peopleGuessed} guessed ● ${remainingTime} seconds left`})
+        
+                    await response.edit({embeds: [flagEmbed]})
+                }
+
+                break;
             }
+            case "leaderboard": {
+                const leaderboardEmbed = new EmbedBuilder()
+                .setColor(embedColors.purple)
+                .setTitle("Flag Quiz - Most Points")
 
-            peopleGuessed = correctUsers.length + wrongUsers.length
-            flagEmbed.setFooter({text: `${peopleGuessed} guessed ● ${remainingTime} seconds left`})
-            
-            await response.edit({embeds: [flagEmbed]})
-            i.deferUpdate()
-        });
+                const gameStats = await db.get(`flags_stats.flag_quiz`)
 
-        buttonCollector.on('end', async () => {
+                if (!gameStats) {
+                    leaderboardEmbed.setDescription("huh, looks like there's nothing here");
+                    await interaction.reply({ embeds: [leaderboardEmbed] });
+                    return;
+                }
 
-            buttonRow.components.forEach(async button => {
-                await button
-                    .setStyle(ButtonStyle.Danger)
-                    .setDisabled(true)
-            })
+                let pointsLeaderboardArray = [];
+                let pointsLeaderboardText = "";
 
-            const rightFlagIndex = shuffledFlags.indexOf(rightFlag)
-            await buttonRow.components[rightFlagIndex].setStyle(ButtonStyle.Success)
+                for (const userId of Object.keys(gameStats)) {
+                    const userInfo = await client.users.fetch(userId)
+                    const userName = userInfo.username
 
-            await response.edit({embeds: [flagEmbed], components: [buttonRow]})
+                    gameStats[userId].name = userName
+                    pointsLeaderboardArray.push(gameStats[userId])
+                }
 
-            const usersFormatter = new Intl.ListFormat("en", { type: "conjunction" })
-            const correctUserString = usersFormatter.format(correctUsers)
-            const wrongUserString = usersFormatter.format(wrongUsers)
+                //sort by most points (kinda confusing)
+                pointsLeaderboardArray.sort((a, b) => {
+                    if (a.points > b.points) return -1
+                    else if (a.points < a.points) return 1
+                    else return 0 
+                });
 
-            let responseText = ""
+                for (user of pointsLeaderboardArray) {
+                    pointsLeaderboardText += `${user.name}: **${user.points} points**\n`
+                }
 
-            switch (correctUsers.length) {
-                case 0:
-                    responseText += `Nobody got it right! \n`
-                    break;
-                case 1:
-                    responseText += `${correctUserString} got it right! gg \n`
-                    break;
-                case 2:
-                    responseText += `${correctUserString} both got it right! gg \n`
-                    break;
-                default:
-                    responseText += `${correctUserString} all got it right! gg \n`
-                    break;
+                leaderboardEmbed.setDescription(pointsLeaderboardText);
+    
+                await interaction.reply({ embeds: [leaderboardEmbed] });
             }
-            switch (wrongUsers.length) {
-                case 0:
-                    responseText += `That means nobody got it wrong... pretty good ig`
-                    break;
-                case 1:
-                    responseText += `That means ${wrongUserString} got it wrong, massive L`
-                    break;
-                case 2:
-                    responseText += `That means ${wrongUserString} both got it wrong, massive L`
-                    break;
-                default:
-                    responseText += `That means ${wrongUserString} all got it wrong, massive L`
-                    break;
-            }
-            
-            await interaction.followUp(responseText)
-        });
-
-        while (remainingTime) {
-            await sleep(5000)
-
-            remainingTime -= 5
-            flagEmbed.setFooter({text: `${peopleGuessed} guessed ● ${remainingTime} seconds left`})
-
-            await response.edit({embeds: [flagEmbed]})
         }
     }
 }
