b0b0e743355402a5a5aba033db1c586f07f2339f
AgentNebulator <mattylaven@gmail.com>
2024-08-19 15:50:38 -0500

    Deleted unnecessary old files

--- old_cmds/settings.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'settings',
-    description: "config command but different",
-    execute({message, args}, vars){
-        require(`./config`).execute({message, args}, vars);
-    }
-}
