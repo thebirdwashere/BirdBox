c17f0c596c9d8875e55da13729f77fca935708fe
AgentNebulator <mattylaven@gmail.com>
2024-10-01 18:20:21 -0500

    fixed issue with message tests

--- utils/scripts/message_tests.js
+++ utils/scripts/message_tests.js
@@ -129,8 +129,7 @@ module.exports = {
             //get the random footer for the embed
             const randomWord = testResult[Math.floor(Math.random() * testResult.length)];
             const randomLetter = randomWord[0].toUpperCase();
-            const randomFooter = footers.alphabetical[Math.floor(Math.random() * footers.alphabetical.length)]
-                .replace("(randomWord)", randomWord).replace("(randomLetter)", randomLetter);
+            const randomFooter = footers.alphabetical[Math.floor(Math.random() * footers.alphabetical.length)].replace("(randomWord)", randomWord).replace("(randomLetter)", randomLetter);
             
             //get alphabetical version
             const alphabeticalString = testResult.join(" ");
@@ -173,14 +172,14 @@ module.exports = {
             let content = message.content.toLowerCase();
 
             if (content.length > 973) return;                             //this checks if the message is empty or too long
-            if (content.match(/[^a-zA-Z\s]/)) return;                      //this tests for non-alphabetical characters
+            if (content.match(/[^a-zA-Z\s]/)) return;                     //this tests for non-alphabetical characters
             if (content.includes("j") || content.includes("q")) return;   //no j or q on the periodic table, fun fact
 
-            if (content.length < 20) return;                           //only test strings with more than 20 characters, for coolness factor
+            if (content.length < 20) return;                              //only test strings with more than 20 characters, for coolness factor
 
-            if (content.endsWith("x")) return;                         //another fun fact: x only starts a two-letter combo, so cannot end it
+            if (content.endsWith("x")) return;                            //another fun fact: x only starts a two-letter combo, so cannot end it
             const impossibleEndings = ["aa", "ad", "ae", "az", "bd", "bg", "bl", "bm", "bt", "bz", "cg", "ct", "cz", "da", "dd", "de", "dg", "dl", "dm", "dr", "dt", "dz", "ea", "ed", "ee", "eg", "el", "em", "et", "ez", "fa", "fd", "fg", "ft", "fz", "gg", "gl", "gm", "gr", "gt", "gz", "ha", "hd", "hl", "hm", "hr", "ht", "hz", "ia", "id", "ie", "ig", "il", "im", "it", "iz", "ka", "kd", "ke", "kg", "kl", "km", "kt", "kz", "ld", "le", "lg", "ll", "lm", "lt", "lz", "ma", "me", "ml", "mm", "mr", "mz", "ng", "nl", "nm", "nr", "nt", "nz", "oa", "od", "oe", "ol", "om", "or", "ot", "oz", "pe", "pg", "pl", "pz", "rd", "rl", "rm", "rr", "rt", "rz", "sa", "sd", "sl", "st", "sz", "td", "tg", "tr", "tt", "tz", "ua", "ud", "ue", "ug", "ul", "um", "ur", "ut", "uz", "va", "vd", "ve", "vg", "vl", "vm", "vr", "vt", "vz", "wa", "wd", "we", "wg", "wl", "wm", "wr", "wt", "wz", "xa", "xb", "xc", "xd", "xf", "xg", "xh", "xi", "xk", "xl", "xm", "xn", "xo", "xp", "xr", "xs", "xt", "xu", "xv", "xw", "xy", "xz", "ya", "yd", "ye", "yg", "yl", "ym", "yr", "yt", "yz", "za", "zb", "zc", "zd", "ze", "zf", "zg", "zh", "zi", "zk", "zl", "zm", "zo", "zp", "zs", "zt", "zu", "zv", "zw", "zy", "zz"]
-            if (impossibleEndings.includes(content.slice(-2))) return;     //all of these also impossible endings to a message (btw i'm bothering to check the ending super well because it's going to take a while to get there with this algorithm, and figuring out which endings are impossible is relatively straightforward with another algoritm i made elsewhere. it's already super fast so i really don't have to do this, but i am bored on winter break and terrified to push this complicated update)
+            if (impossibleEndings.includes(content.slice(-2))) return;    //all of these also impossible endings to a message (btw i'm bothering to check the ending super well because it's going to take a while to get there with this algorithm, and figuring out which endings are impossible is relatively straightforward with another algoritm i made elsewhere. it's already super fast so i really don't have to do this, but i am bored on winter break and terrified to push this complicated update)
 
             const ones = ["h","b","c","n","o","f","p","s","k","v","y","i","w","u"]; 
             const twos = ["he","li","be","ne","na","mg","al","si","cl","ar","ca","sc","ti","cr","mn","fe","co","ni","cu","zn","ga","ge","as","se","br","kr","rb","sr","zr","nb","mo","tc","ru","rh","pd","ag","cd","in","sn","sb","te","xe","cs","ba","la","ce","pr","nd","pm","sm","eu","gd","tb","dy","ho","er","tm","yb","lu","hf","ta","re","os","ir","pt","au","hg","tl","pb","bi","po","at","rn","fr","ra","ac","th","pa","np","pu","am","cm","bk","cf","es","fm","md","no","lr","rf","db","sg","bh","hs","mt","ds","rg","cn","nh","fl","mc","lv","ts","og"];
@@ -237,7 +236,7 @@ module.exports = {
             }
         },
         respond: async ({message, vars, testResult}) => {
-            const randomFooter = footers.periodic[Math.floor(Math.random() * footers.periodic.length)];
+            const randomFooter = footers.periodic[Math.floor(Math.random() * footers.periodic.length)].replace("(userName)", message.author.username);
 
             const periodicString = testResult;
             
@@ -285,7 +284,7 @@ module.exports = {
             return content
         },
         respond: async ({message, vars, testResult}) => {
-            const randomFooter = footers.pangrams[Math.floor(Math.random() * footers.pangrams.length)].replace("(username)", message.author.username);
+            const randomFooter = footers.pangrams[Math.floor(Math.random() * footers.pangrams.length)].replace("(userName)", message.author.username);
 
             const pangramString = testResult;
             
