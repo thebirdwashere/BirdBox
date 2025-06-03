ecffc4ad4257d54faa134fa9ae61b403ddbf6ec4
AgentNebulator <160374436+AgentNebulator@users.noreply.github.com>
2025-06-03 10:24:20 -0500

    rr command

---
+++ src/commands/fun/rr.ts
@@ -0,0 +1,19 @@
+import { Command } from "src/utility/command.js";
+import { EmbedBuilder } from "discord.js";
+
+const RR = new Command({
+    name: "rr",
+    description: "I don't know what this command does, you should try it.",
+    execute: async (ctx) => {
+        const rrEmbed = new EmbedBuilder()
+            .setColor(0xFFFFFF)
+            .setTitle("Never Gonna Give You Up")
+            .setAuthor({ name: "Rick Astley", iconURL: "https://i1.sndcdn.com/artworks-000222761718-he77qt-t500x500.jpg" })
+            .setDescription("We're no strangers to love \nYou know the rules and so do I \nA full commitment's what I'm thinking of \nYou wouldn't get this from any other guy \nI just wanna tell you how I'm feeling \nGotta make you understand \n\nNever gonna give you up \nNever gonna let you down \nNever gonna run around and desert you \nNever gonna make you cry \nNever gonna say goodbye \nNever gonna tell a lie and hurt you \n\nWe've known each other for so long \nYour heart's been aching but you're too shy to say it \nInside we both know what's been going on \nWe know the game and we're gonna play it \nAnd if you ask me how I'm feeling \nDon't tell me you're too blind to see \n\nNever gonna give you up \nNever gonna let you down \nNever gonna run around and desert you \nNever gonna make you cry \nNever gonna say goodbye \nNever gonna tell a lie and hurt you")
+            .setFooter({text: "lol get rekt u been rick rolled, there you go, you happy now?!? please dont do that too much, it is literally spam city here now."});
+
+        await ctx.reply({ embeds: [rrEmbed] });
+    }
+});
+
+export default RR;
