e0d898643f4a239d24f85214fe2b79cc17299872
AgentNebulator <160374436+AgentNebulator@users.noreply.github.com>
2025-06-03 11:06:03 -0500

    ping changes

--- src/commands/tools/ping.ts
+++ src/commands/tools/ping.ts
@@ -5,10 +5,11 @@ const Ping = new Command({
   name: "ping",
   description: "Pings the bot and returns the response.",
   execute: async (ctx) => {
+    // console.log(ctx.data.client);
 		const pingEmbed = new EmbedBuilder()
 			.setColor(0x5282EC)
 			.addFields(
-				{ name: "Ping Time", value: "Currently Not Implemented"}
+				{ name: "Ping Time", value: `${ctx.data.client.ws.ms ?? "Undefined "}ms`}
 			)
 			.setFooter({ text: "pong you bumbling pillock" });
 
