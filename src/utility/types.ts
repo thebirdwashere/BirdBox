8ce83e1f3e72e6cca88a9a5473b548e43cec35c2
AgentNebulator <160374436+AgentNebulator@users.noreply.github.com>
2025-06-04 18:09:13 -0500

    8ball command

--- src/utility/types.ts
+++ src/utility/types.ts
@@ -1,19 +1,16 @@
 import { Client } from "discord.js";
 import { CommandRegistry } from "./command.js";
 
-export interface Perms {
-  host: Record<string, number>;
-  developer: Record<string, number>;
-  contributor: Record<string, number>;
-  botTester: Record<string, number>;
-}
-
 export interface Data {
   prefix: string;
   perms: Perms;
   registry: CommandRegistry;
   client: Client;
-}
+};
+
+export interface Footers {
+  magic8ball: string[];
+};
 
 export type PatchNotes = {
   version: string;
@@ -23,8 +20,19 @@ export type PatchNotes = {
   notes: string[];
 }[];
 
+export interface Perms {
+  host: Record<string, number>;
+  developer: Record<string, number>;
+  contributor: Record<string, number>;
+  botTester: Record<string, number>;
+};
+
 export interface Options {
   number: Map<string, number | null>;
   boolean: Map<string, boolean | null>;
   string: Map<string, string | null>;
 }
+
+export interface Responses {
+  magic8ball: (string | {text: string; credit?: string; url?: string; image?: string;})[];
+};
