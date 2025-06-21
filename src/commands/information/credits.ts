81fa2abce6d3c49ac5d0ab9b4af421a0076aaecc
AgentNebulator <160374436+AgentNebulator@users.noreply.github.com>
2025-06-21 18:22:02 -0500

    credits fixes

--- src/commands/information/credits.ts
+++ src/commands/information/credits.ts
@@ -1,13 +1,14 @@
 import { Command } from "src/utility/command.js";
-import { EmbedBuilder } from "discord.js";
+import { EmbedBuilder, Colors } from "discord.js";
+
 const Credits = new Command({
-  name: "credits",
-  description: "Give some due credit to the people who worked on this bot.",
-  execute: async (ctx) => {
+  	name: "credits",
+  	description: "Give some due credit to the people who worked on this bot.",
+  	execute: async (ctx) => {
 		const perms = ctx.data.perms;
 
         const creditsEmbed = new EmbedBuilder()
-			.setColor(0xFFFFFF)
+			.setColor(Colors.White)
 			.setTitle("Credits")
 			.setDescription("Credits to the people who work/worked on this bot.")
 			.addFields(
