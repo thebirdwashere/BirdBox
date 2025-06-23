a34c82d77d37657d3618b856f2a019d4ab47a016
AgentNebulator <160374436+AgentNebulator@users.noreply.github.com>
2025-06-23 15:11:24 -0500

    cleaned up echo command

--- src/commands/tools/echo.ts
+++ src/commands/tools/echo.ts
@@ -2,18 +2,17 @@ import { Command, CommandOption } from "src/utility/command.js";
 
 const Echo = new Command({
   name: "echo",
-  description: "Pings the bot and returns the response.",
+  description: "BirdBox replicates a custom message. Not too useful, but certainly fun!",
   options: [
     new CommandOption({
       name: "message",
-      description: "the message you want the bot to respond with",
+      description: "The message to echo.",
       type: "string",
     }),
   ],
   execute: async (ctx, opts) => {
     const message = opts.string.get("message");
-    if (message === undefined || message === null)
-      throw new Error("Could not retrieve message.");
+    if (message == null) throw new Error("Could not retrieve message.");
 
     await ctx.reply(message);
   },
