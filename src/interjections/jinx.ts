98336cbd4ada1be53b005ea5f7afafce55da08a0
AgentNebulator <160374436+AgentNebulator@users.noreply.github.com>
2026-05-06 18:23:13 -0500

    added jinx detection

---
+++ src/interjections/jinx.ts
@@ -0,0 +1,28 @@
+import { MessageContext } from "src/utility/context.js";
+import { Interjection } from "src/utility/interjection.js";
+
+const JINX_WINDOW = 3000;
+
+const Jinx = new Interjection({
+  name: "jinx",
+  test: async (ctx: MessageContext) => {
+    if (!ctx.channel) return;
+
+    const previousMessages = await ctx.channel.messages.fetch({limit:2});
+    const lastMessage = previousMessages.last();
+
+    if (lastMessage === undefined) return;
+
+    //the required tests
+    const jinxCreatedCloseTogether = Math.abs(lastMessage.createdTimestamp - ctx.message.createdTimestamp) <= JINX_WINDOW;
+    const contentIsIdentical = lastMessage.content === ctx.message.content;
+    const jinxFromDifferentPeople = lastMessage.author.id !== ctx.message.author.id;
+
+    //only pass if all true
+    if (jinxCreatedCloseTogether && contentIsIdentical && jinxFromDifferentPeople) {
+      await ctx.send(lastMessage.content);
+    }
+  }
+});
+
+export default Jinx;
