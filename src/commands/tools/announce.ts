220e7bdc343e3e14279e0d02387918d3f62daad7
AgentNebulator <160374436+AgentNebulator@users.noreply.github.com>
2026-05-11 16:32:03 -0500

    renamed announce

--- src/commands/tools/announce.ts
+++ src/commands/tools/announce.ts
@@ -1,5 +1,6 @@
 import { Command, CommandOption } from "src/utility/command.js";
 import { EmbedBuilder } from "discord.js";
+import { fetchConfigOption } from "src/utility/utility.js";
 
 const Announce = new Command({
   name: "announce",
@@ -12,8 +13,8 @@ const Announce = new Command({
     }),
     new CommandOption({
       name: "channel",
-      description: "A link to the channel announced to. If not set, defaults to server-configured default channel.",
-      type: "string",
+      description: "The channel to be announced to. If not set, defaults to the server-configured default channel.",
+      type: "channel",
       optional: true,
     }),
   ],
@@ -22,15 +23,20 @@ const Announce = new Command({
     if (ctx.guild === null) throw new Error("Command must be run in a server.");
     if (ctx.channel === null || !("name" in ctx.channel)) throw new Error("Command must be run in a valid channel.");
 
-    //TODO: Add config support
-    const announce_channel_id = opts.string.get("channel")?.replace(`https://discord.com/channels/${ctx.guild.id}/`, "") /*?? await getSettingValue(`settings.announce_channel.${message.guildId}`, db) ?? message.channelId*/;
+    let announceChannelId = opts.channel.get("channel")?.id ?? fetchConfigOption(ctx.db, "server", "announcements", ctx.guild.id);
+    //autocomplete suggested this why. this looks awful. what is this. ig its compact tho
+    announceChannelId ??= ctx.channel.id;
 
-    if (announce_channel_id === undefined) throw new Error("Announcement channel ID could not be found.");
+    if (typeof announceChannelId !== "string") 
+      throw new Error("No announcement channel provided, or provided data was not a channel ID.");
 
-    const announce_channel = ctx.guild.channels.cache.get(announce_channel_id);
+    const announceChannel = ctx.guild.channels.cache.get(announceChannelId);
 
-    if (announce_channel === undefined) throw new Error("Provided ID could not be located. Ensure the requested channel exists in this server.");
-    if (!("send" in announce_channel)) throw new Error("Provided channel does not support sent messages.");
+    if (announceChannel === undefined) 
+      throw new Error("Provided ID could not be located. Ensure the requested channel exists in this server.");
+    
+    if (!("send" in announceChannel)) 
+      throw new Error("Provided channel does not support sent messages.");
 
     const announceEmbed = new EmbedBuilder()
       .setTitle(`${ctx.channel.name ?? "undefined"}'s Announcement`)
@@ -39,7 +45,7 @@ const Announce = new Command({
       .setColor(0xAA00FF)
       .setTimestamp(ctx.timestamp);
 
-    announce_channel.send({ embeds: [announceEmbed] })
+    announceChannel.send({ embeds: [announceEmbed] })
       .then(async () => {
         await ctx.reply("Announcement sent!");
       }).catch(() => {
