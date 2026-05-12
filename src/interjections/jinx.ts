816c64889ee50dba6471c239c5a184d1593ba9ed
AgentNebulator <160374436+AgentNebulator@users.noreply.github.com>
2026-05-11 22:05:46 -0500

    optimized jinx using sqlite

--- src/interjections/jinx.ts
+++ src/interjections/jinx.ts
@@ -1,25 +1,72 @@
 import { Interjection } from "src/utility/interjection.js";
+import { createHash } from "crypto";
+import { DatabaseSync } from "node:sqlite";
 
 const JINX_WINDOW = 3000;
+
+interface jinxMessage {
+  contentHash: string,
+  timestamp: number,
+  authorId: string,
+}
+
+const db = new DatabaseSync(":memory:");
+db.exec(`
+  CREATE TABLE IF NOT EXISTS JinxData(
+    channel_id TEXT PRIMARY KEY NOT NULL,
+    data TEXT DEFAULT '{}'
+  )
+`);
+
+const dbRequest = db.prepare(`
+  SELECT data 
+  FROM JinxData
+  WHERE (channel_id == @id)
+  LIMIT 1
+`);
+
+const dbUpdate = db.prepare(`
+  INSERT INTO JinxData (channel_id, data)
+  VALUES (@id, @data)
+  ON CONFLICT(channel_id) DO 
+  UPDATE SET data=excluded.data
+`);
 
 const Jinx = new Interjection({
   name: "jinx",
   test: async (ctx) => {
+    console.log("tset running");
     if (!ctx.channel) return;
+    const channelId = ctx.channel.id;
 
-    const previousMessages = await ctx.channel.messages.fetch({limit:2});
-    const lastMessage = previousMessages.last();
+    const lastData = dbRequest.get({ id: channelId });
 
-    if (lastMessage === undefined) return;
+    const contentHash = createHash("SHA256").update(ctx.message.content).digest("base64");
+
+    const authorId = ctx.user.id;
+    const timestamp = ctx.message.createdTimestamp;
+
+    dbUpdate.run({ 
+      id: channelId, 
+      data: JSON.stringify({
+        contentHash,
+        timestamp,
+        authorId,
+      })
+    });
+
+    if (lastData === undefined) return;
+
+    const lastMessage = JSON.parse(lastData.data?.toString() ?? "{}") as jinxMessage;
 
     //the required tests
-    const jinxCreatedCloseTogether = Math.abs(lastMessage.createdTimestamp - ctx.message.createdTimestamp) <= JINX_WINDOW;
-    const contentIsIdentical = lastMessage.content === ctx.message.content;
-    const jinxFromDifferentPeople = lastMessage.author.id !== ctx.message.author.id;
+    const jinxCreatedCloseTogether = Math.abs(lastMessage.timestamp - ctx.message.createdTimestamp) <= JINX_WINDOW;
+    const contentIsIdentical = lastMessage.contentHash === contentHash;
+    const jinxFromDifferentPeople = lastMessage.authorId !== authorId;
 
     //only pass if all true
     if (jinxCreatedCloseTogether && contentIsIdentical && jinxFromDifferentPeople) {
-      await ctx.send(lastMessage.content);
+      await ctx.send(ctx.message.content);
     }
   }
 });
