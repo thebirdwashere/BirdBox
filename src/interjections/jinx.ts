4c1af529e04f6938e65f69fec1bced887c406a6e
AgentNebulator <160374436+AgentNebulator@users.noreply.github.com>
2026-05-14 17:55:37 -0500

    jinx optimizations

--- src/interjections/jinx.ts
+++ src/interjections/jinx.ts
@@ -1,37 +1,15 @@
 import { Interjection } from "src/utility/interjection.js";
-import { createHash } from "crypto";
-import { DatabaseSync } from "node:sqlite";
 import { fetchConfigOption } from "src/utility/utility.js";
 
 const JINX_WINDOW = 3000;
 
-interface jinxMessage {
-  contentHash: string,
+interface JinxMessage {
+  content: string,
   timestamp: number,
   authorId: string,
 }
 
-const db = new DatabaseSync(":memory:");
-db.exec(`
-  CREATE TABLE IF NOT EXISTS JinxData(
-    channel_id TEXT PRIMARY KEY NOT NULL,
-    data TEXT DEFAULT '{}'
-  )
-`);
-
-const dbRequest = db.prepare(`
-  SELECT data 
-  FROM JinxData
-  WHERE (channel_id == @id)
-  LIMIT 1
-`);
-
-const dbUpdate = db.prepare(`
-  INSERT INTO JinxData (channel_id, data)
-  VALUES (@id, @data)
-  ON CONFLICT(channel_id) DO 
-  UPDATE SET data=excluded.data
-`);
+const jinxData: Record<string, JinxMessage> = {};
 
 const Jinx = new Interjection({
   name: "jinx",
@@ -42,28 +20,21 @@ const Jinx = new Interjection({
       if (!jinxSetting) return false;
     }
 
-    const lastData = dbRequest.get({ id: ctx.channel.id });
+    const lastData = jinxData[ctx.channel.id] as JinxMessage | undefined;
 
-    const contentHash = createHash("SHA256").update(ctx.message.content).digest("base64");
-
-    dbUpdate.run({ 
-      id: ctx.channel.id, 
-      data: JSON.stringify({
-        contentHash,
-        timestamp: ctx.message.createdTimestamp,
-        authorId: ctx.user.id,
-      })
-    });
+    jinxData[ctx.channel.id] = {
+      content: ctx.message.content,
+      timestamp: ctx.message.createdTimestamp,
+      authorId: ctx.user.id,
+    };
 
     if (lastData === undefined) return false;
 
-    const lastMessage = JSON.parse(lastData.data?.toString() ?? "{}") as jinxMessage;
-
     //only pass if all true
     if (
-      Math.abs(lastMessage.timestamp - ctx.message.createdTimestamp) <= JINX_WINDOW
-      && lastMessage.contentHash === contentHash 
-      && lastMessage.authorId !== ctx.user.id
+      Math.abs(lastData.timestamp - ctx.message.createdTimestamp) <= JINX_WINDOW
+      && lastData.content === ctx.message.content 
+      && lastData.authorId !== ctx.user.id
     ) {
       await ctx.send(ctx.message.content);
       return true;
