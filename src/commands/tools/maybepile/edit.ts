6c9ebc3c0cc4edde032972fee5fe1f698bdf21f4
AgentNebulator <160374436+AgentNebulator@users.noreply.github.com>
2026-05-16 12:17:20 -0500

    rewrote maybepile edit to use new modal system

--- src/commands/tools/maybepile/edit.ts
+++ src/commands/tools/maybepile/edit.ts
@@ -1,4 +1,4 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, ComponentType, ModalBuilder, ModalSubmitInteraction, TextInputBuilder, TextInputStyle } from "discord.js";
+import { ActionRowBuilder, Message, ModalBuilder, ModalSubmitInteraction, TextInputBuilder, TextInputStyle } from "discord.js";
 import { Subcommand, CommandOption } from "src/utility/command.js";
 import { MaybepileEntry } from "src/utility/types.js";
 import { getMaybepile, getPageNumber, maybepileAutocomplete, updateMaybepile } from "./utils.js";
@@ -74,56 +74,21 @@ const MaybepileEdit = new Subcommand({
       .setTitle("Edit Maybepile Item")
       .addComponents(modalFields);
 
-    const editButton = new ButtonBuilder()
-      .setCustomId("maybepile-edit-button")
-      .setLabel("Edit")
-      .setStyle(ButtonStyle.Success);
-    const buttonRow = new ActionRowBuilder<ButtonBuilder>()
-      .addComponents(editButton);
+    async function onModalSubmit(i: ModalSubmitInteraction, _: Message): Promise<void> {
+      const newItem: MaybepileEntry = {
+        title: i.fields.getTextInputValue("maybepile-title"),
+        description: i.fields.getTextInputValue("maybepile-description"),
+        suggester: i.fields.getTextInputValue("maybepile-suggester"),
+        claim: originalClaim,
+      };
 
-    const response = await ctx.reply({ components: [buttonRow]});
+      pileArray[itemNum] = newItem;
+      updateMaybepile(ctx.db, pileArray);
 
-    const buttonCollector = response.createMessageComponentCollector({
-      componentType: ComponentType.Button,
-      time: 15000,
-    });
-
-    async function handleButtonInteraction(i: ButtonInteraction): Promise<void> {
-      await i.showModal(editModal);
-      
-      const editFilter = (i: ModalSubmitInteraction): boolean => (
-        i.user.id === ctx.user.id
-        && i.customId === "maybepile-edit"
-      );
-      await i.awaitModalSubmit({ filter: editFilter, time: 120_000 })
-        .then(
-          async i => {
-            const newItem: MaybepileEntry = {
-              title: i.fields.getTextInputValue("maybepile-title"),
-              description: i.fields.getTextInputValue("maybepile-description"),
-              suggester: i.fields.getTextInputValue("maybepile-suggester"),
-              claim: originalClaim,
-            };
-
-            pileArray[itemNum] = newItem;
-            updateMaybepile(ctx.db, pileArray);
-
-            await i.reply({content: `"${pileArray[itemNum].title}" has been edited successfully!`});
-          }
-        );
-    }
-    
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    buttonCollector.on("collect", async (i) => {await handleButtonInteraction(i);});
-
-    async function handleButtonTimeout(): Promise<void> {
-      //disable the buttons
-      buttonRow.components.forEach(item => item.setDisabled(true));
-      await response.edit({ components: [buttonRow] });
+      await i.reply({content: `"${pileArray[itemNum].title}" has been edited successfully!`});
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    buttonCollector.on("end", async () => {await handleButtonTimeout();});
+    await ctx.replyModal(editModal, onModalSubmit);
   },
 });
 
