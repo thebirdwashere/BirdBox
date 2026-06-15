3857d806b5864acd2a214499e8c1df75550e8c17
AgentNebulator <160374436+AgentNebulator@users.noreply.github.com>
2026-06-15 17:39:58 -0500

    initial tests

---
+++ src/commands/games/snake.ts
@@ -0,0 +1,129 @@
+import { Command } from "@src/utility/command.js";
+import { sleep } from "@src/utility/utility.js";
+import { EmbedBuilder } from "discord.js";
+
+const GRID_SIZE = 10;
+const INITAL_SNAKE_HEAD: [number, number] = [1, 1];
+const INITAL_SNAKE_TAIL: [number, number] = [1, 0];
+
+const BLANK_TILE = "⬛";
+const SNAKE_HEAD_TILE = "🟢";
+const SNAKE_BODY_TILE = "🟩";
+const FRUIT_TILE = "🍎";
+
+const Snake = new Command({
+  name: "snake",
+  description: "The classic snake game, playable (if only barely) on BirdBox!",
+  execute: async (ctx) => {
+    const game = new SnakeGame();
+
+    const snakeEmbed = new EmbedBuilder()
+      .setTitle("Snake")
+      .setDescription(game.renderGrid());
+
+    const response = await ctx.reply({embeds: [snakeEmbed]});
+
+    await sleep(1000);
+
+    game.step();
+    snakeEmbed.setDescription(game.renderGrid());
+
+    await response.reply({embeds: [snakeEmbed]});
+  },
+});
+
+class SnakeGame {
+  grid: string[][];
+  lastSnakeHead: [number, number];
+  snakeHead: [number, number];
+  lastSnakeTail: [number, number];
+  snakeTail: [number, number];
+  snakeDirection: "n" | "s" | "w" | "e";
+  gameOver: boolean;
+
+  renderGrid(): string {
+    return this.grid.map(row => row.join("")).reduce((acc, curr) => acc + curr + "\n", "");
+  }
+
+  drawTo(xy: [number, number], tile: string): void {
+    this.grid[xy[0]][xy[1]] = tile;
+  }
+
+  drawSnake(): void {
+    this.drawTo(this.lastSnakeHead, SNAKE_BODY_TILE);
+    this.drawTo(this.snakeHead, SNAKE_HEAD_TILE);
+    this.drawTo(this.lastSnakeTail, BLANK_TILE);
+  }
+
+  drawAndGrowSnake(): void {
+    this.drawTo(this.lastSnakeHead, SNAKE_BODY_TILE);
+    this.drawTo(this.snakeHead, SNAKE_HEAD_TILE);
+  }
+
+  step(): boolean {
+    let newHeadCoords: [number, number], newTailCoords: [number, number];
+
+    switch (this.snakeDirection) {
+    case "n": {
+      newHeadCoords = [this.snakeHead[0] - 1, this.snakeHead[1]];
+      newTailCoords = [this.snakeTail[0] - 1, this.snakeTail[1]];
+      break;
+    } case "s": {
+      newHeadCoords = [this.snakeHead[0] + 1, this.snakeHead[1]];
+      newTailCoords = [this.snakeTail[0] + 1, this.snakeTail[1]];
+      break;
+    } case "e": {
+      newHeadCoords = [this.snakeHead[0], this.snakeHead[1] + 1];
+      newTailCoords = [this.snakeTail[0], this.snakeTail[1] + 1];
+      break;
+    } case "w": {
+      newHeadCoords = [this.snakeHead[0], this.snakeHead[1] - 1];
+      newTailCoords = [this.snakeTail[0], this.snakeTail[1] - 1];
+      break;
+    }
+    }
+
+    if (!newHeadCoords.concat(newTailCoords).flat().every(num => 
+      num >= 0 && num < GRID_SIZE
+    )) {
+      this.gameOver = true;
+      return false;
+    }
+
+    this.lastSnakeHead = this.snakeHead;
+    this.lastSnakeTail = this.snakeTail;
+    this.snakeHead = newHeadCoords;
+    this.snakeTail = newTailCoords;
+
+    const targetTile = this.grid[newHeadCoords[0]][newHeadCoords[1]];
+    if (targetTile === SNAKE_BODY_TILE || targetTile === SNAKE_HEAD_TILE) {
+      this.gameOver = true;
+      return false;
+    } else if (targetTile === FRUIT_TILE) {
+      this.drawAndGrowSnake();
+    } else {
+      this.drawSnake();
+    }
+
+    return true;
+  }
+
+  constructor() {
+    //https://stackoverflow.com/questions/53992415/how-to-fill-multidimensional-array-in-javascript
+    this.grid = Array(GRID_SIZE).fill([]).map((): string[] => Array(GRID_SIZE).fill(BLANK_TILE) as string[]);
+    
+    this.lastSnakeHead = INITAL_SNAKE_HEAD;
+    this.snakeHead = INITAL_SNAKE_HEAD;
+    this.lastSnakeTail = INITAL_SNAKE_TAIL;
+    this.snakeTail = INITAL_SNAKE_TAIL;
+    this.snakeDirection = "e";
+    this.gameOver = false;
+
+    this.drawTo(this.snakeHead, SNAKE_HEAD_TILE);
+    this.drawTo(this.snakeTail, SNAKE_BODY_TILE);
+
+    this.drawTo([1, 2], FRUIT_TILE);
+  }
+}
+
+export default Snake;
